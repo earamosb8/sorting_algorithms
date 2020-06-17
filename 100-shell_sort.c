7d17259e27f54c6ae95deb1aa14a8fcdcd1b2d05
Camilo Moncada <monk-da@hotmail.com>
2020-06-17 12:34:49 -0500

    response question 4

---
+++ 100-shell_sort.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+
+/**
+ * shell_sort - sort list with shell sort
+ * @array: a list for sort
+ * @size: size the list
+ **/
+
+void shell_sort(int *array, size_t size)
+{
+	int inner, outer, valueToInsert;
+	int interval = 1;
+	int elements = size;
+	int i = 0;
+
+	if (array == NULL)
+		return;
+
+	while (interval <= elements / 3)
+	{
+		interval = interval * 3 + 1;
+	}
+
+	while (interval > 0)
+	{
+
+		for (outer = interval; outer < elements; outer++)
+		{
+			valueToInsert = array[outer];
+			inner = outer;
+
+			while (inner > interval - 1 && array[inner - interval]
+			       >= valueToInsert)
+			{
+				array[inner] = array[inner - interval];
+				inner -= interval;
+			}
+
+			array[inner] = valueToInsert;
+		}
+		print_array(array, size);
+
+		interval = (interval - 1) / 3;
+		i++;
+
+	}
+}
--- sort.h
+++ sort.h
@@ -24,4 +24,6 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void recursion_qs(int arr[], int low, int high, size_t size);
 int partition(int *arr, int low, int high, size_t size);
+void shell_sort(int *array, size_t size);
+
 #endif
